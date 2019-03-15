2e1e7e3cc96a039f4e3166b86b4d09c243e5fa3b
rafael-junio <rafael-954@hotmail.com>
2019-03-15 18:41:16 -0400

    Loading the palette and DMA

--- Snes_Init.asm
+++ Snes_Init.asm
@@ -3,10 +3,10 @@
  	clc 	 	; clear carry to switch to native mode
  	xce 	 	; Xchange carry & emulation bit. native mode
  	rep 	#$18 	; Binary mode (decimal mode off), X/Y 16 bit
-         ldx 	#$1FFF  ; set stack to $1FFF
-         txs
- 
-         jsr Init
+    ldx 	#$1FFF  ; set stack to $1FFF
+    txs
+
+    jsr Init
  .ENDM
  
  .bank 0
--- source.asm
+++ source.asm
@@ -1,38 +1,55 @@
-; SNES Initialization Tutorial code
-; This code is in the public domain.
+.include "Header.inc"
+.include "Snes_Init.asm"
  
- .include "Header.inc"
- .include "Snes_Init.asm"
+.section "TileData"
+	.include "tiles\tiles.inc"
+.ends
+
+.Macro LoadPalette
+			lda #\2							;Needs to understand what the #\2 means
+			sta $2121
+			lda #:\1
+			ldx #\1
+			ldy #(3 * 2)
+			jsr DMAPalette
+.endm
+
+; Needed to satisfy interrupt definition in "Header.inc".
+VBlank:
+  RTI
  
- ; Needed to satisfy interrupt definition in "Header.inc".
- VBlank:
-   RTI
- 
- .bank 0
- .section "MainCode"
- 
- Start:
+.bank 0
+.section "MainCode"
+Start:
 
-        lda #%00011111      ; load red (0000000000011111) on acumulator (low byte)
-        sta $2122	
-        stz $2122           ; second byte has no data, so we write a 0 (high byte)
+   			lda #%00011111      ; load red (0000000000011111) on acumulator (low byte)
+  		    sta $2122			; store from acumulator on low byte of $2122
+    		stz $2122           ; second byte has no data, so we write a 0 (high byte)
+  		    sta $2100           ; Turn on screen, full brightness
+			stz $2122			; Store 0 to memory $2122 (low byte)
+			stz $2122			; Store 0 to the second byte of $2122 (high byte)
 
-        sta $2100           ; Turn on screen, full brightness
+Forever:
+	    jmp Start
 
-				stz $2122						; Store 0 to memory $2122 (low byte)
-				stz $2122						; Store 0 to the second byte of $2122 (high byte)
+DMAPalette:
+			phb								;push data bank register
+			php								;push processor status register
+			
+			stx $4302						;store the ldx #\1 (data offset) in the $4302 (DMA source address channel 0)
+			sta $4304						;store data bank of lda in $4304 (DMA source address channel 0)
+			sty $4305						;store syze data into $4305 (DMA number of bytes to transfer)
 
-				stz $2100						; Store 0 to turn off the screen
-				lda #%11111111			; low byte white
-				sta $2122
-				lda #%01111111			; high byte white
-				sta $2122
-				
-				sta $2100
-	
-	
+			;initialize de DMA
+			stz $4300						;store zero on the DMA control register
+			lda #$22						;load on acumulator the number 22 to load it on the DMA 43x1 in the format ($21xx)
+			sta $4301						;store $2122 (CGRAM) on DMA destination register
+			lda #$01						;load 01 on acumulator
+			sta $420B						;Start the DMA (with 01 from acumulator)
 
- Forever:
-     jmp Start
- 
- .ends
+			plb 
+			plb 
+
+			rts 								;return
+
+.ends
