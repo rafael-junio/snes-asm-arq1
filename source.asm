ab9161152d97f99d40ade62b49ba617b6b37b531
rafael-junio <rafael-954@hotmail.com>
2019-04-12 20:45:14 -0400

    Moving tiles

--- source.asm
+++ source.asm
@@ -4,9 +4,11 @@
 
 
 .equ PalletteNum $0000 			; Variable with the pallette starter
+.equ hexXPos $0001
+.equ hexYPos $0002
 
 .Macro Stall
-	.rept 50
+	.rept 10
 		WAI
 	.endr
 .endm
@@ -22,11 +24,12 @@ Start:
 	sep #$20
 
 	stz PalletteNum				; Store 0 on variable
+	stz hexXPos
+	stz hexYPos
 
-	LoadPalette BG_Palette, 0, 14
+	LoadPalette BG_Palette, 0, 16
 
-	LoadBlockToVRAM Tiles, $0000, $0020 
-
+	LoadBlockToVRAM Tiles, $0000, $0040 
 
 	lda #$80
 	sta $2115					; Set the VRAM Address Increment Mode to after acessing high byte
@@ -48,6 +51,24 @@ Forever:
     adc #$04
     and #$0C        ; If palette starting color > 28 (00011100), make 0
     sta PalletteNum
+
+	lda hexXPos
+	clc
+	sbc #$01
+	sta hexXPos
+
+	lda hexYPos
+	clc
+	sbc #$04
+	sta hexYPos
+
+	lda hexXPos					
+	sta $210D					; $210D BG1 Horizontal Scroll
+	sta $210D
+
+	lda hexYPos					
+	sta $210E					; $210E BG1 Vertical Scroll
+	sta $210E
 
 	jmp Forever
 
@@ -88,19 +109,15 @@ SetupVideo:
 
 	lda #$04					; in binary 8 bits, 6 for the address of the tile and two for the size (aaaaaass)					
 	sta $2107					; BG1 Tile map Location
+
+	lda #$04
+	sta $2108					; BG2 Tile map Location
 	
-	stz $210B					; Tile location for BG1 and BG2 (11112222)
+	lda #$20
+	sta $210B					; Tile location for BG1 and BG2 (22221111)
 
-	lda #$01					
-	sta $212C					; Enable BG1
-
-	lda #$81					; 81 in HEX
-	sta $210D					; $210D BG1 Horizontal Scroll
-	sta $210D
-
-	lda #$81					; FF in HEX
-	sta $210E					; $210E BG1 Vertical Scroll
-	sta $210E
+	lda #$03					
+	sta $212C					; Enable BG1 and BG2
 
 	lda #$0F
 	sta $2100					; Turn the screen on
@@ -117,6 +134,9 @@ SetupVideo:
 		.DW $0000,$0000,$0000,$0000,$0000,$0000,$0000,$0000
 		.DB $7C,$00,$BA,$7C,$82,$7C,$7C,$00
 		.DB $10,$00,$D6,$00,$7C,$00,$38,$00
+	Tiles2:
+		.db $FF, $00, $FF, $00, $FF, $00, $FF, $00, $FF, $00, $FF, $00, $FF, $00, $FF, $00
+    	.db $FF, $00, $DB, $00, $DB, $00, $DB, $00, $FF, $00, $7E, $00, $00, $00, $FF, $00
 
 	BG_Palette:
 	.db $00, $00, $C5, $4D, $09, $4B, $98, $FF, $0C, $2C, $0D, $FF, $81, $00, $CA, $00
